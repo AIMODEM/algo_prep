ab436b844776b15e0f20918248a10a004fc6b02e
AIMODEM <aijpshm@gmail.com>
2023-05-21 12:49:32 +0530

    added validate_subsequence

---
+++ Arrays_28/2_Validate_Subsequence.py
@@ -0,0 +1,22 @@
+# Solution 1:
+# O(n) time | O(1) space
+def isValidSubsequenceSolution1(array, sequence):
+    arrIdx = 0
+    seqIdx = 0
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+    return seqIdx == len(sequence)
+
+# Solution 2:
+# O(n) time | O(1) time 
+def isValidSubsequence(array, sequence):
+    seqIdx = 0
+    for value in array:
+        if seqIdx == len(sequence):
+            break
+        if sequence[seqIdx] == value:
+            seqIdx += 1
+    return seqIdx == len(sequence)
+
